55180d6354eebc818638f8060aea20a61f1ab5af
gitrate <htmlfau@gmail.com>
2017-03-07 01:27:42 +0200

    Lesson_13, main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // Awaiting document loading;
-$(document).ready(function () {
-	console.log("Ready 1");
-});
+// Есть три способа дождаться, пока документ загрузится, если у нас нет defer (неразборчиво на ТЭ, над ЭЙ, на DAY, на THEY - лекция js_13, timing  1:51:00) Мы можем обернуть весь код в одну из этих записей ниже:
 
-$(document).on("ready", function () {
-	console.log("Ready 2");
-});
+// $(document).ready(function () {
+	// console.log("Ready 1");
+// });
 
-$(function () {
-	console.log("Ready 3");
-})
+// $(document).on("ready", function () {
+	// console.log("Ready 2");
+// });
+
+// $(function () {
+	// console.log("Ready 3");
+// });
 
 // Navigation
 var mainTitle = $("h1.mainTitle");
@@ -17,11 +19,11 @@ var spanText = $("span.text");
 
 // api.jquery.com
 
-// console.log(mainTitle.children);
+// console.log(mainTitle.children());
 // console.log(mainTitle.children("input"));
 // console.log(mainTitle.parent());
 // console.log(mainTitle.parent("div"));
-// console.log(mainTitle.parents);
+// console.log(mainTitle.parents());
 // console.log(mainTitle.parents("body"));
 // console.log(mainTitle.find("*"));
 // console.log(mainTitle.find(".text"));
@@ -37,5 +39,39 @@ var spanText = $("span.text");
 // mainTitle.removeClass("test");
 // console.log(mainTitle.hasClass("test"));
 // mainTitle.toggleClass("test");
+// console.log(mainTitle.hasClass("test"));
 
-console.log($("img").attr("src"));
+// console.log($("img").attr("src"));
+// $("img").attr("src", "images/tasd.gif");
+// $("img").removeAttr("src");
+
+// console.log($("img").attr("src"));
+// $("img").attr("src", "asd");
+// $("img").removeProp("src");
+
+// console.log(mainTitle.html());
+// console.log(mainTitle[0].innerHTML);
+// mainTitle.html("<div>text</div>");
+// mainTitle[0].innerHTML = "<div>text</div>";
+// console.log($("<div>text</div>"));
+
+// var clickableButton = $("<div>clickable Button</div>")
+		// .css("color", "red")
+		// .on("click", function () {alert("asd")} );
+		// mainTitle.find("span.text").html(clickableButton);
+
+// console.log($("input").val());
+// $("input").val("test");
+// console.log($("input").val());
+
+
+
+
+
+
+
+
+
+
+
+
