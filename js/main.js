85e7ebf4279d16a53f032347b97932bfa7e0c86c
gitrate <htmlfau@gmail.com>
2017-03-07 17:30:57 +0200

    lecture_14, JQuery style manipulation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,88 @@
-var dataElem = $("['data-test']");
-console.log(dataElem.data("test"));
-console.log(dataElem.data("test2"));
+// jQuery манипуляции атрибутами
+
+var dataElem = $("[data-test]");
+// console.log(dataElem.data("test"));
+// console.log(dataElem.data("test2"));
+// dataElem.data("test", "new value");
+// console.log(dataElem.data("test"));
+// dataElem.data({
+	// "test": "new value test",
+	// "test2": "new value test 2"
+// });
+// console.log(dataElem.data("test"));
+// console.log(dataElem.data("test2"));
+// dataElem.data("test2", {isValid: true});
+// console.log(dataElem.data("test2"));
+// dataElem.removeData("test2");
+// console.log(dataElem.data("test2"));
+
+// dataElem.removeAttr("data-test");
+// dataElem.removeAttr("data-test2");
+// dataElem.removeData(["data-test", "data-test2"]);
+// console.log(dataElem.data("test"));
+// console.log(dataElem.data("test2"));
+
+
+// jQuery манипуляция стилями
+
+var logoImage = $(".logo");
+// console.log(logoImage.css("width"));
+// console.log(logoImage.css(["position", "height"]));
+// logoImage.css({
+	// "position": "relative",
+	// "height": "200px"
+// });
+// console.log(logoImage.css(["position", "height"]));
+// logoImage.css("width", "200px");
+// console.log(logoImage.css("width"));
+// // console.log(window.getComputedStyle(logoImage[0]));
+// console.log($(":radio:eq(0)")[0].value);
+// console.log($(":radio:eq(2)")[0].value);
+// console.log($(":radio:eq(2)").val());
+// // $(":radio:eq(2)")[0].value === $(":radio:eq(2)").val()
+
+
+// console.log(logoImage.height());
+// logoImage.height(200);
+// console.log(logoImage.height());
+
+// console.log(logoImage.width());
+// logoImage.width(200);
+// console.log(logoImage.width());
+
+// logoImage.width("10rem");
+// console.log(logoImage.css("width"));
+// console.log(logoImage.width());
+
+// console.log(logoImage.innerWidth());
+// logoImage.innerWidth(200);
+// console.log(logoImage.width());
+// console.log(logoImage.innerWidth());
+
+// console.log(logoImage.outerWidth());
+// console.log(logoImage.outerWidth(true));
+// logoImage.outerWidth(200);
+// console.log(logoImage.outerWidth());
+// logoImage.outerWidth(200, true);
+// console.log(logoImage.outerWidth());
+
+
+// jQuery позиционирование элементов
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
